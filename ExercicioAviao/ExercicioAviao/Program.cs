d092d79936c0f226177478f9a1ce7f22db75564d
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-10-10 16:23:57 -0300

    Atualizado dia 10/10/2022

---
+++ ExercicioAviao/ExercicioAviao/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExercicioAviao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ExercicioFamilia/ExercicioFamilia/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ExercicioFamilia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoaFilho = new Pessoa();
+            
+            pessoaFilho.idade = 12;
+            
+            pessoaFilho.nome = "Diego";
+            
+            Pessoa pessoaMae = new Pessoa();
+            
+            pessoaMae.nome = "Erika";
+            
+            pessoaMae.idade = 35;
+            
+            Pessoa pessoaPai = new Pessoa();
+            
+            pessoaPai.nome = "Sergio";
+            
+            pessoaPai.idade = 31;
+            
+            Pessoa pAvoPai = new Pessoa();
+            
+            pAvoPai.nome = "João";
+           
+            pAvoPai.idade = 62;
+            
+            pessoaPai.pai = pAvoPai;
+           
+            pessoaFilho.mae = pessoaMae;
+            
+            pessoaFilho.pai = pessoaPai;
+        }
+    }
+}
