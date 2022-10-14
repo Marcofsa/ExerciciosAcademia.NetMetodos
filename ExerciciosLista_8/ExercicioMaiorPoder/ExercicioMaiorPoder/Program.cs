ac4d175fb92b392a341e9221bd12b2cd4f846e5c
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-10-14 16:16:51 -0300

    14/10/2022

---
+++ ExerciciosLista_8/ExercicioMaiorPoder/ExercicioMaiorPoder/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExercicioMaiorPoder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+
+            Personagem personagem1 = new Personagem();
+            Personagem personagem2 = new Personagem();
+            Personagem personagem3 = new Personagem();
+            
+        }
+    }
+}
