95d8f2473db06779029caaf582a044c6960a3cfb
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-10-14 13:34:53 -0300

    14/10/2022

--- ExercicioPessoa/ExercicioPessoa/Program.cs
+++ ExercicioPessoa/ExercicioPessoa/Program.cs
@@ -4,22 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe o nome da pessoa: ");
+            Console.WriteLine("|------------------------------|");
+            Console.WriteLine("   Informe o nome da pessoa: ");
+            Console.WriteLine("|------------------------------|");
             string nome = Console.ReadLine();
-
-            Console.WriteLine("Informe a idade da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
+            Console.WriteLine("   Informe a idade da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
             int idade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe o peso da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
+            Console.WriteLine("    Informe o peso da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
             double peso = double.Parse(Console.ReadLine());
-
-            Console.WriteLine("Informe a altura da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
+            Console.WriteLine("   Informe a altura da pessoa: ", nome);
+            Console.WriteLine("|------------------------------|");
             double altura = double.Parse(Console.ReadLine());
 
             string sexo;
             do
             {
-                Console.WriteLine("Informe o sexo da pessoa: (M)asculino ou (F)eminino:", nome);
+                Console.WriteLine("|----------------------------------------------------------|");
+                Console.WriteLine("  Informe o sexo da pessoa: (M)asculino ou (F)eminino:", nome);
+                Console.WriteLine("|----------------------------------------------------------|");
                 sexo = Console.ReadLine().ToUpper();
                 if (sexo == "M")
                 {
@@ -34,21 +41,24 @@
 
                 else
                 {
-
+                    Console.WriteLine("|----------------------------------------------------------|");
                     Console.WriteLine("Esse sexo não existe!\nUtilize apenas 'M' ou 'F'! ");
-
+                    Console.WriteLine("|----------------------------------------------------------|");
                 }
 
 
             } while (sexo != "M" && sexo != "F");
-
-            Console.WriteLine("Informe a cor dos olhos de: ", nome);
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("   Informe a cor dos olhos de: ", nome);
+            Console.WriteLine("|----------------------------------|");
             string olhos = Console.ReadLine();
-
-            Console.WriteLine("Informe a cor dos cabelos de: ", nome);
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("    Informe a cor dos cabelos de: ", nome);
+            Console.WriteLine("|----------------------------------|");
             string cabelos = Console.ReadLine();
-
-            Console.WriteLine("Informe a raça de: ", nome);
+            Console.WriteLine("|----------------------------------|");
+            Console.WriteLine("    Informe a raça de: ", nome);
+            Console.WriteLine("|----------------------------------|");
             string raca = Console.ReadLine();
 
             Pessoa pessoa1 = new Pessoa(nome, idade, peso, altura, sexo, olhos, cabelos, raca);
