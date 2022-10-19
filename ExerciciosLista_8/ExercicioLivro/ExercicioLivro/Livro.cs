828b363c32583f3914c715b199f4af696206bbae
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-10-19 16:31:51 -0300

    ATUALIZADO DIA 19/10/2022

---
+++ ExerciciosLista_8/ExercicioLivro/ExercicioLivro/Livro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioLivro
+{
+    internal class Livro
+    {
+        class Livros
+        {
+            private string _titulo { get; set; }
+            private int _paginas { get; set; }
+
+            public Livros() { }
+            public Livros(string titulo, int paginas)
+            {
+                Titulo = titulo;
+                Paginas = paginas;
+            }
+            public string Titulo { get => _titulo; set => _titulo = value; }
+            public int Paginas { get => _paginas; set => _paginas = value; }
+        }
+    }
+}
---
+++ ExerciciosLista_8/ExercicioLivro/ExercicioLivro/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace ExercicioLivro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> livros = new Dictionary<string, int>();
+            int option;
+
+            do
+            {
+                Console.WriteLine(" >>>> SISTEMA DE REGISTRO DE LIVROS <<<< \n");
+                Console.WriteLine("1 - Registrar um livro");
+                Console.WriteLine("2 - Listar os livros");
+                Console.WriteLine("3 - Procurar um livro");
+                Console.WriteLine("4 - Sair do programa");
+                Console.Write("\nSelecione uma opção: ");
+                option = int.Parse(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Digite o nome do livro: ");
+                        string livro = Console.ReadLine();
+                        Console.Write($"Quantas páginas tem o livro '{livro}': ");
+                        int paginas = int.Parse(Console.ReadLine());
+
+                        livros.Add(livro, paginas);
+                        Console.WriteLine($"'{livro}' cadastrado com sucesso!\n");
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("*** Livros cadastrados ***");
+                        foreach (var l in livros)
+                        {
+                            Console.WriteLine($"Titulo: {l.Key}  |  Páginas: {l.Value}");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.Write("Informe o título do livro a ser localizado: ");
+                        string titulo = Console.ReadLine();
+
+                        var contem = livros.FirstOrDefault((livros) => (livros.Key.Equals(titulo)));
+
+                        if (contem.Key != null)
+                        {
+                            Console.WriteLine($"Titulo: {contem.Key}   |   Páginas: {contem.Value}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nTítulo não encontrado. Verifique a ortografia e tente novamente.\nObs.: Maiúsculos e minúsculos são diferentes.\n");
+                        }
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("Essa opção não é válida!\n");
+                        break;
+                }
+            } while (option != 4);
+        }
+    }
+}
