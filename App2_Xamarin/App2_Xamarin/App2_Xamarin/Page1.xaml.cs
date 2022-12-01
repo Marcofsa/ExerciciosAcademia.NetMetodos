34a7298c64c92b00e072816c9051ca24006461a5
Marcofsa <87910108+Marcofsa@users.noreply.github.com>
2022-12-01 20:09:07 -0300

    Xamarin

---
+++ App2_Xamarin/App2_Xamarin/App2_Xamarin/Page1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            botaoExecutar.Clicked += botaoExecutar_Clicked;
+        }
+
+         private void botaoExecutar_Clicked(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(entradaValor.Text);
+            string resp = "O número " + entradaValor.Text + "é: ";
+            if (n%2 == 0)
+            {
+                resp += "par";
+            }
+            else
+            {
+                resp += "ímpar";
+            }
+            labelResposta.Text = resp;
+        }
+    }
+}
